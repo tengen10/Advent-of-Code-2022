c199ba261cb62a50d28120697aaa09bc8181c343
Todd Engen <engentm@outlook.com>
2022-12-04 16:18:33 -0600

    Add files via upload

---
+++ day1.ps1
@@ -0,0 +1,24 @@
+﻿# Advent of Code Day 1
+
+# Part 1 & 2
+
+$groups = new-object "system.collections.arraylist"
+
+$list = Get-Content .\input.txt
+
+foreach ($i in $list) {
+$sum += $i
+if ($i -eq '') {
+    [void]$groups.add($sum)
+    $sum = 0
+    }
+}
+
+
+# Part 1 solution
+$groups | Sort-Object -Descending | Select-Object -First 1 
+
+# Part 2 solution
+$groups | sort-object -Descending | Select-Object -First 3 | Measure-Object -Sum
+
+
